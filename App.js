e3d8496e993393566490091137c499f9526d56b7
Doris Cheng <dorcheng@umich.edu>
2017-11-18 18:43:31 -0500

    initial setup of potate

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>P O T A T E</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+## About Potate
+
+"Haters gonna hate, potatoes gonna potate' - Anonymous Potato #93
+
+Potate is a mobile app that tracks a user's laziness. It was created with [Create React Native App](https://github.com/react-community/create-react-native-app).
+
+## More to come...
