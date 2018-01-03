dc91351bb19cf6a22885db799dd57579ba29dca6
Doris Cheng <dorcheng@umich.edu>
2018-01-03 02:07:06 -0500

    edited README and added ability for users to edit their name and email

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 "Haters gonna hate, potatoes gonna potate' - Anonymous Potato #93
 
-Potate is a mobile app that tracks a user's laziness. It was created with [Create React Native App](https://github.com/react-community/create-react-native-app).
+Potate is a mobile app that tracks a user's laziness. It was created with [Create React Native App](https://github.com/react-community/create-react-native-app) and is fully compatible with both Android & iOS thanks to [React Native Elements](https://github.com/react-native-training/react-native-elements). [Firebase](https://github.com/firebase/firebase-js-sdk) was used for the backend (server & user authentication).
 
-## More to come...
+## A brief walkthrough of Potate's current functionalities
+
+When a user first opens Potate, he/she will come across the Login page. The page will prompt the user to either sign in using an existing account or to sign up with a new account. New users will be required to fill out four main fields: first name, last name, email, and password.
+
+After logging into Potate, the user will see his/her user profile. The top will show his/her full name, a potato image, and the user's current level (all new users begin with an image of a baby potato and their level set to 0). Users can navigate to see their friends and add new friends who are other users of Potate. Users can also navigate to their settings to change account information.
+
+A yellow button labeled 'POTATE' is located underneath the profile. This button navigates to the user's timeline where he/she can begin to 'potate' (a.k.a. add lazy events to his/her timeline). The more events he or she adds, the more badges/awards he or she can earn.
+
+## Further updates to come
+
+This mobile app is still in development. Further updates will include a user's ability to navigate to an awards screen to view all the badges/awards he or she has earned. There will also be an activity page which serves as the user's newsfeed. Events such as earning a new badge will pop up here. The newsfeed will also show events that happen to the user's friends (such as leveling up, daily statuses, etc.)
--- component/Settings.js
+++ component/Settings.js
@@ -1,13 +1,116 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import { firebaseRef, database } from '../server/firebase'
+import { ScrollView, Text, Picker } from 'react-native'
+import { FormLabel, FormInput, Button } from 'react-native-elements'
+import { editName } from '../reducers/userInfo'
 
 
-export default class Settings extends Component {
+class Settings extends Component {
+  constructor(props){
+    super(props)
+    console.log('PROPS', props)
+    this.state = {
+      user: firebaseRef.auth().currentUser,
+      firstName: null,
+      lastName: null,
+      email: null,
+      status: 'SAVE'
+    }
+    this.onSave = this.onSave.bind(this)
+  }
+
+  onSave(user){
+    const { firstName, lastName, status, email } = this.state
+
+    if (status === 'SAVE' && firstName !== null || lastName !== null || email !== null){
+      this.setState({status: 'SAVED!'})
+    }
+
+    let update = firstName && lastName ? {firstName, lastName} : firstName ? {firstName} : lastName ? {lastName} : null
+
+    if (update !== null) {
+      database.ref(`users/${user.uid}`).update(update)
+      .then(() => {
+        setTimeout(() => this.setState({status: 'SAVE'}), 3000)
+      })
+    }
+
+    if (email !== null){
+      user.updateEmail(email).then(function() {
+        this.setState({status: 'SAVE'})
+      }).catch(function(error) {
+        console.log(error)
+        this.setState({status: 'ERROR'})
+      })
+    }
+  }
+
+  onDelete(user){
+    user.delete().then(function() {
+      console.log('Successfully deleted account')
+    }).catch(function(error) {
+      console.log(error)
+    });
+  }
+
   render() {
+    console.log('settings state', this.state)
     return (
       <ScrollView style={{backgroundColor: '#252530'}}>
-        <Text>Settings</Text>
+        <FormLabel labelStyle={{fontSize: 17, color: '#AC9C90'}}>First</FormLabel>
+        <FormInput
+          defaultValue={this.props.info.firstName}
+          inputStyle={{fontSize: 16}}
+          onChangeText={(firstName) => this.setState({firstName})}
+          underlineColorAndroid="#ffde88"
+          tintColor={'#ffde88'}
+          containerStyle={{marginLeft: 18}}
+          placeholder={this.props.info.firstName}
+          placeholderTextColor={'#4F4B49'}
+          selectTextOnFocus={true}
+        />
+        <FormLabel labelStyle={{fontSize: 17, color: '#AC9C90'}}>Last</FormLabel>
+        <FormInput
+          defaultValue={this.props.info.lastName}
+          inputStyle={{fontSize: 16}}
+          onChangeText={(lastName) => this.setState({lastName})}
+          underlineColorAndroid="#ffde88"
+          tintColor={'#ffde88'}
+          containerStyle={{marginLeft: 18}}
+          placeholder={this.props.info.lastName}
+          placeholderTextColor={'#4F4B49'}
+          selectTextOnFocus={true}
+        />
+        <FormLabel labelStyle={{fontSize: 17, color: '#AC9C90'}}>Email</FormLabel>
+        <FormInput
+          defaultValue={this.props.user.email}
+          inputStyle={{fontSize: 16}}
+          onChangeText={(email) => this.setState({email})}
+          underlineColorAndroid="#ffde88"
+          tintColor={'#ffde88'}
+          containerStyle={{marginLeft: 18}}
+          placeholder={this.props.user.email}
+          placeholderTextColor={'#4F4B49'}
+          selectTextOnFocus={true}
+        />
+        <Button
+          small
+          raised
+          iconRight={{name: 'emoticon-excited', type:'material-community', color:'#282834'}}
+          backgroundColor={'#ffde88'}
+          borderRadius={30}
+          textStyle={{color:'#282834', fontSize:18, fontWeight:'bold'}}
+          containerViewStyle={{marginTop: 25, width: 250, borderRadius: 30, alignSelf: 'center'}}
+          buttonStyle={{height: 35}}
+          title={this.state.status}
+          onPress={() => this.onSave(this.state.user)}
+        />
       </ScrollView>
     );
   }
 }
+
+const mapStateToProps = (state) => ({user: state.user, info: state.info})
+
+export default connect(mapStateToProps)(Settings)
